0df29d22e456b40a957d7ba6bd25a041ab604404
MarcelliaNr <marcellianr1234@gmail.com>
2022-04-03 21:51:04 +0700

    commit conditional

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -75,4 +75,64 @@ void main() {
   print(a < b);
   print(a >= b);
   print(a <= b);
+
+  //conditional
+  print("conditional");
+  var nilai;
+  nilai = 80;
+
+  if (nilai >= 80) {
+    print("A");
+  } else if (nilai <= 80 && nilai >= 50) {
+    print("B");
+  } else {
+    print("Tidak Lulus");
+  }
+
+  print("---------");
+  nilai >= 80 ? print("A") : print("Tidak A");
+
+  //function
+  print("function");
+
+  hitungnilai();
+  hitungnilai1(75, 90);
+  var p = hitungnilai1(70, 100, 55);
+  print(p);
+  var n = hitungnilai2(mapel1: 50, mapel2: 2);
+  print(n);
+  hitungnilai3(72, 100);
+}
+
+//function
+hitungnilai() {
+  print("hitung nilai");
+}
+
+//positional arggument
+hitungnilai1(mapel1, mapel2, [mapel3]) {
+  var nilaiakhir1;
+  if (mapel3 != null) {
+    nilaiakhir1 = mapel1 / mapel2 + mapel3;
+  } else {
+    nilaiakhir1 = mapel1 / mapel2;
+  }
+  return nilaiakhir1;
+}
+
+//name arggument
+hitungnilai2({mapel1, mapel2}) {
+  var nilaiakhir2;
+  if (mapel2 != null) {
+    nilaiakhir2 = mapel1 / mapel2;
+  } else {
+    nilaiakhir2 = mapel1;
+  }
+  return nilaiakhir2;
+}
+
+//void
+void hitungnilai3(mapel1, mapel2) {
+  var nilaiakhir3 = mapel1 + mapel2;
+  print(nilaiakhir3);
 }
